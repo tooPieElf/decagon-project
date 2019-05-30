e83af08b73f9dfba25516a6e0dc7ffc69b010cfa
chukwudi <ndimnezechukwudi@gmail.com>
2019-05-30 17:10:17 +0000

    create working backend

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -75,9 +75,74 @@ $(document).ready(function () {
 
     $("#welcome-section button.update").click( function() {
 
-                $("#welcome-section #amen").remove();
+        $("#welcome-section #amen").remove();
 
+         var message= `<div id="amen">
+         <h1> Please enter your details</h1>
+         <div class="login-box">
+             <form >
+ 
+                 <div>
+                     <input class="login-box__input" type="text" id="name" placeholder="Name" required="required">
+                 </div>
+ 
+                 <div>
+                     
+                     <input class="login-box__input" placeholder="Email"  id="email" type="email" required="required">
+                 </div>
+ 
+                 <div>
+                    
+                     <input class="login-box__input" placeholder="Phone number" type="tel" id="phone" required="required">
+                 </div>
+ 
+                 <div>
+                     
+                     <input class="login-box__input" placeholder="Password" type="password" id="password" required="required">
+                 </div>
+ 
+ 
+                 <div class="button">
+                     <button type="button" id="update">update</button>
+                 </div>
+             </form>`
 
+             $("#welcome-section").append(message);
+
+             let user = {
+                name: $('input#name').val(),
+                Email: $('input#email').val(),
+                phone: $('input#phone').val(),
+                password: $('input#password').val()
+
+            }
+            var name = $('input#name').val(),
+                Email = $('input#email').val(),
+                phone = $('input#phone').val(),
+                password = $('input#password').val();
+
+                $("#update").on('click', function(){
+
+                   
+
+                if (name == '' || Email == '' || phone == '' || password == '') {
+                    alert("please fill in all form fields");
+                    alert(name);
+                }
+                    
+                
+                    
+                   
+                
+            
+                });    
+
+            
+        
+
+             
+
+             
 
         
             });
