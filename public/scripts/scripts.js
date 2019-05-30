650096b71eaf1d70715f533ed6b6c5195bd5be58
chukwudi <ndimnezechukwudi@gmail.com>
2019-05-30 08:50:13 +0000

    create backend connection

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    var newWindow = window.open("login.html");
+    
 
 
     $("#submitt").on('click', function () {
@@ -11,32 +11,55 @@ $(document).ready(function () {
             password: $('input#password').val()
 
         }
+var name= $('input#name').val(), Email= $('input#email').val(), phone= $('input#phone').val(), password= $('input#password').val()
 
+if(name==''||Email==''||phone==''||password=='')
+        {
+        alert("please fill in all form fields");
+        }else{
 
-
-        $.ajax({
-            type: 'POST',
-            url: "http://localhost:3000/posts",
-            data: user,
-            success: function (response) {
-                alert(response);
-            }
-        });
+            $.ajax({
+                type: 'POST',
+                url: "http://localhost:3000/posts",
+                data: user,
+                success: function (response) {
+                    window.location.href = "welcome.html";
+                }
+            });
+        }
+        
 
 
 
     })
 
-    // $("#login").on('click', function(){
-    //     $.ajax({
-    //         type: 'GET',
-    //         url: "http://localhost:3000/posts",
+    
 
-    //         success: function(response){
-    //             alert(response);
-    //         }
-    //     });
+    $("#login").on('click', function(){
+        let email = $('#email').val();
+        let password = $('#password').val();
+        
+        $.ajax({
+            type: 'GET',
+            url: "http://localhost:3000/posts",
 
-    // })
+            success: function(response){
+                $.each(response, function(key, value){
+                    
+                    if(email==value.Email && password==value.password){
+                        window.location.href = "welcome.html";
+                    
+                    }
+                        
+                    
+                })
+                alert('not found')
+                
+            }
+        });
+    
+
+    })
+
 
 })
